3cc0aecd3b3621c1af9aadc5330cd6f88e709de2
YaroslavNykolyshyn <slawik123nikolischn@gmail.com>
2021-02-11 10:17:43 +0200

    homework1.3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,15 @@
 package com.company;
+import java.lang.Math;
+import java.util.Random;
+
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int[][] array = new int[5][7];
+        int a = (int)(Math.random()*(75+1))-2;
+            System.out.println(a);
+        int b = (int)(Math.random()*(75+1))-2;
+        System.out.println(b);
+        }
     }
-}
